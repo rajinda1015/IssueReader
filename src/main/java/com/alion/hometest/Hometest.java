1528a512e3a25d0ff90dc2437395db0a9ee6c9e4
Rajinda Amaranath Dissanayake <55923327+rajinda1015@users.noreply.github.com>
2021-05-29 20:01:39 +0530

    Initial commit

---
+++ src/main/java/com/alion/hometest/Hometest.java
@@ -0,0 +1,40 @@
+package com.alion.hometest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableScheduling
+public class Hometest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hometest.class, args);
+	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.alion.hometest.controller")).build();
+	}
+
+	/**
+	 * Configure rest template
+	 * 
+	 * @param builder
+	 * @return
+	 */
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+}
